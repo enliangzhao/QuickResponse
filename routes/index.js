297538c8e6fb7fdd71ed68a64b04dab2c5295363
Damin Xu <36313350+ricardodxu@users.noreply.github.com>
2021-11-22 23:30:31 -0500

    add "share report" page

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,9 @@ router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Quick Responser' });
 });
 
+router.get('/post_report', function(req, res, next) {
+  console.log("redirect to report");
+  res.render('report');
+});
+
 module.exports = router;

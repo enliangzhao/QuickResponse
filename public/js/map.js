565b9c5e80257f3b1d75625c32680e0f4d050055
Damin Xu <36313350+ricardodxu@users.noreply.github.com>
2021-11-24 00:17:10 -0500

    222

---
+++ public/js/map.js
@@ -0,0 +1,29 @@
+
+
+// google map api setup
+// Initialize and add the map
+function initMap(locations) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 12,
+            center: currentLocation,
+            });
+            const current_location = new google.maps.Marker({
+                position: currentLocation,
+                map,
+                title: "current location"
+            });
+            current_location.addListener("click", () => {
+                alert("marker: current location");
+            });
+            var iconUrl;
+            // for (var i = 0; i < locations.length; i++) {
+            //     newMarker(map, locations[i]);
+            // }
+        });
+    }else{
+        alert("Error: cannot get current location");
+    }
+}

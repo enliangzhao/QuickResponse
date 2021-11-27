a75de1c72db0d47e78de260f6702c76873fc5fe2
Damin Xu <36313350+ricardodxu@users.noreply.github.com>
2021-11-27 15:47:32 -0800

    update

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,37 @@
+const l1 = {position: {lat:37.40884747971561, lng: -122.06000685125716},
+     label: "Crime 1",
+     title: "Murder here! 2021,1,1"};
+const l2 = {position: {lat:37.409993741493295, lng: -122.06358518842138},
+     label: "describe Crime 2"};
+const l3 = {position: {lat:37.40998147620404, lng: -122.06286383452085},
+     label: "describe Crime 3"};
+const locations = [l1,l2,l3];
+
+
+function newMarker(map, location){
+    console.log(location.position.lat)
+    console.log(location.position.lng)
+    var temp;
+    temp = new google.maps.Marker({
+        position: location.position,
+        map,
+        label: {text: location.label, color: "green"},
+        title: location.title
+    });
+
+    temp.addListener("click", () => {
+        alert(temp.getTitle())
+        // $.ajax({
+        //     type: "POST",
+        //     url: 'http://localhost:3000/createReport',
+        //     data: {},
+        //     success: function(data){
+        //         alert(data)
+        //     }
+        // });
+    });
+}
+
 
 
 // google map api setup
@@ -17,22 +51,25 @@ function initMap(locations) {
                 label: {text: "You", color: "white"},
                 title: "current location"
             });
-            current_location.addListener("click", () => {
-                $.ajax({
-                    type: "POST",
-                    url: 'http://localhost:3000/createReport',
-                    data: {},
-                    success: function(data){
-                        alert(data)
-                    }
-                  });
-            });
-            var iconUrl;
-            // for (var i = 0; i < locations.length; i++) {
-            //     newMarker(map, locations[i]);
-            // }
+
+            
+            // current_location.addListener("click", () => {
+            //     $.ajax({
+            //         type: "POST",
+            //         url: 'http://localhost:3000/createReport',
+            //         data: {},
+            //         success: function(data){
+            //             alert(data)
+            //         }
+            //       });
+            // });
+            for (var i = 0; i < locations.length; i++) {
+                 newMarker(map, locations[i]);
+            }
         });
     }else{
         alert("Error: cannot get current location");
     }
 }
+
+initMap(locations)
